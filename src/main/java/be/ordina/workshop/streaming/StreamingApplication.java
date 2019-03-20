ea733c2a466db0cfc71ef0ac8c800931b27bb6f1
Tim Ysewyn <Tim.Ysewyn@me.com>
2019-03-20 18:48:38 +0200

    Upgraded to latest and greatest & got rid of embedded broker - please use docker

--- src/main/java/be/ordina/workshop/streaming/StreamingApplication.java
+++ src/main/java/be/ordina/workshop/streaming/StreamingApplication.java
@@ -2,21 +2,12 @@ package be.ordina.workshop.streaming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.test.rule.KafkaEmbedded;
 
 @SpringBootApplication
 public class StreamingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StreamingApplication.class, args);
-	}
-
-	@Bean
-	public KafkaEmbedded embeddedKafka() {
-		KafkaEmbedded embeddedKafka = new KafkaEmbedded(1, true, 1);
-		embeddedKafka.setKafkaPorts(9092);
-		return embeddedKafka;
 	}
 
 }
